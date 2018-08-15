435064b41e4ead0d623010294658079cf8c9f6e2
pankajpansari <pankaj.pansari1@gmail.com>
2018-08-15 11:05:04 +0100

    Added function to compute dpp objective. Has same class methods and variables as social network influence.

---
+++ dpp_objective.py
@@ -0,0 +1,76 @@
+import networkx as nx
+import numpy as np
+import sys
+import time
+import random
+import torch
+import os
+import math
+import itertools as it
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+from read_files import read_dpp
+
+random.seed(1234)
+
+class DPP(object):
+    def __init__(self, L):
+        self.cache = {}
+        self.cache_hits = 0
+        self.L = L
+        self.itr_total = 0
+        self.itr_new = 0
+        self.itr_cache = 0
+
+    def cache_reset(self):
+        self.cache.reset()
+        self.cache_hits = 0
+
+    def counter_reset(self):
+        self.itr_total = 0
+        self.itr_new = 0
+        self.itr_cache = 0
+
+    def __call__(self, sample):
+
+        key = sample.tobytes()
+        self.itr_total += 1 
+        if key not in self.cache:
+            self.itr_new += 1 
+            val = getDet(self.L, sample)
+            self.cache[key] = val
+        else:
+            self.itr_cache += 1 
+            self.cache_hits += 1
+
+        return self.cache[key]
+
+###########################################
+def getDet(L, sample):
+
+    N = L.shape[0]
+    this_set = torch.LongTensor([i for i in range(N) if sample[i] == 1] )
+
+    if this_set.nelement() == 0:
+        return torch.Tensor([1])
+
+    subRowsMatrix = L.index_select(0, this_set)
+    subMat = subRowsMatrix.index_select(1, this_set)
+
+    temp = subMat.data.numpy()
+    detVal = np.linalg.det(temp)
+    temp = np.array([detVal])
+    return torch.from_numpy(temp)
+
+ 
+def main():
+    N = 100 
+    L = read_dpp("/home/pankaj/Sampling/data/input/dpp/data/dpp_100_1.5_0.5_200_0_0.1_5.h5", N, '/dpp_0')
+    sample = torch.rand(N) > 0.8
+
+    dpp_obj = DPP(L)
+
+    print dpp_obj(sample.numpy())
+
+if __name__ == '__main__':
+    main()
