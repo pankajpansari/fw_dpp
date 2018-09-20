cc5783e4d5c227f1ff85df482c2f8621e587a5be
pankajpansari <pankaj.pansari1@gmail.com>
2018-09-20 12:54:53 +0100

    Major changes. Adapted the frank_wolfe file from social networks to dpps. Simplified the call script. Simple frank-wolfe runs well.

--- call_frank_wolfe.py
+++ call_frank_wolfe.py
@@ -3,7 +3,8 @@ import numpy as np
 import math
 import torch
 from torch.autograd import Variable
-from frank_wolfe_importance import runImportanceFrankWolfe
+#from frank_wolfe_importance import runImportanceFrankWolfe
+from frank_wolfe import runFrankWolfe
 from read_files import read_dpp
 #from variance import convex_var
 import time
@@ -12,65 +13,52 @@ import argparse
 from dpp_objective import DPP 
 np.random.seed(1234)
 
-def call_FrankWolfe(N, dpp, dpp_id, k, nsamples_mlr, num_fw_iter,  if_herd, if_sfo_gt, a, torch_seed):
+wdir = './workspace'
 
-    dirw = './workspace'
+def call_FrankWolfe(dpp, args):
 
-    temp = dirw + '/fw_log_N_' + str(N) + '_' + str(dpp_id) 
+    args_list = [args.torch_seed, args.dpp_id, args.N, args.k, args.num_samples_mc, args.num_fw_iter]
 
-    log_file = '_'.join(str(x) for x in [temp, k, nsamples_mlr, num_fw_iter, if_herd, if_sfo_gt, a, torch_seed]) + '.txt'
+    file_prefix = wdir + '/dpp_' + '_'.join([str(x) for x in args_list])
 
-    temp = dirw + '/fw_opt_N_' + str(N) + '_' + str(dpp_id) 
+    log_file = file_prefix + '_fw_simple_log.txt'
+    opt_file = file_prefix + '_fw_simple_opt.txt'
+    iterates_file = file_prefix + '_fw_simple_iterates.txt'
 
-    opt_file = '_'.join(str(x) for x in [temp, k, nsamples_mlr, num_fw_iter, if_herd, if_sfo_gt, a, torch_seed]) + '.txt'
-
-    temp = dirw + '/iterates_N_' + str(N) + '_' + str(dpp_id) 
-
-    iterates_file = '_'.join(str(x) for x in [temp, k, nsamples_mlr, num_fw_iter, if_herd, if_sfo_gt, 0, torch_seed]) + '.txt'
-
-    #No convex combination for now
-    x_good = torch.Tensor([0]*N)
-    a = 0
-
-    x_opt = runImportanceFrankWolfe(dpp, nsamples_mlr, k, log_file, opt_file, iterates_file, num_fw_iter, if_herd, x_good, a)
+    x_opt = runFrankWolfe(dpp, args, log_file, opt_file, iterates_file)
 
 def main():
 
     tic = time.clock()
 
     parser = argparse.ArgumentParser(description='Running Frank-Wolfe on given DPP')
-    parser.add_argument('N', help='Number of items in DPP', type=int)
-    parser.add_argument('dpp_id', help='Id of dpp file', type=int)
-    parser.add_argument('k', help='Cardinality constraint', type=int)
-    parser.add_argument('nsamples_mlr', help='Number of samples for multilinear relaxation estimation', type=int)
-    parser.add_argument('num_fw_iter', help='Number of iterations of Frank-Wolfe', type=int)
-    parser.add_argument('if_herd', help='True if herding', type=int)
-    parser.add_argument('if_sfo_gt', help='True if greedy ground-truth to be used during importance sampling', type=int)
-    parser.add_argument('a', help='Convex combination coefficient', type=float)
     parser.add_argument('torch_seed', help='Torch random seed', type=int)
     parser.add_argument('dpp_file', help='DPP file', type=str)
+    parser.add_argument('dpp_id', help='Id of dpp file', type=int)
+    parser.add_argument('N', help='Number of items in DPP', type=int)
+    parser.add_argument('k', help='Cardinality constraint', type=int)
+    parser.add_argument('num_samples_mc', help='Number of samples for multilinear relaxation estimation', type=int)
+    parser.add_argument('num_fw_iter', help='Number of iterations of Frank-Wolfe', type=int)
 
     args = parser.parse_args()
     
     N = args.N
     dpp_id = args.dpp_id
     k = args.k #cardinality constraint
-    nsamples_mlr = args.nsamples_mlr #draw these many sets from x for multilinear relaxation
+    num_samples_mc = args.num_samples_mc #draw these many sets from x for multilinear relaxation
     num_fw_iter = args.num_fw_iter 
-    if_herd = args.if_herd
-    if_sfo_gt = args.if_sfo_gt
-    a = args.a
+    if_herd = 0 
     torch_seed = args.torch_seed
 
     torch.manual_seed(torch_seed) 
 
     dpp_file = "/home/pankaj/Sampling/data/input/dpp/data/" + args.dpp_file
 
-    (qualities, features) = read_dpp('/home/pankaj/Sampling/data/input/dpp/data/clustered_dpp_10_2_20_2_1_5_2.h5', 10, 'dpp_2') 
-#    (qualities, features) = read_dpp(dpp_file, N, '/dpp_' + str(dpp_id)) 
+    (qualities, features) = read_dpp(dpp_file, 'dpp_' + str(dpp_id)) 
+
     dpp = DPP(qualities, features)
 
-    call_FrankWolfe(N, dpp, dpp_id, k, nsamples_mlr, num_fw_iter, if_herd, if_sfo_gt, a, torch_seed)
+    call_FrankWolfe(dpp, args)
 
     print "Compeleted in " + str(time.clock() - tic) + 's'
 
--- frank_wolfe.py
+++ frank_wolfe.py
@@ -1,16 +1,21 @@
-import networkx as nx
+from read_files import read_dpp
+from dpp_objective import DPP 
 import sys
 import numpy as np
 import math
 import torch
-from influence import ic_model as submodObj
-from influence import Influence 
 from torch.autograd import Variable
 import logger
 from builtins import range
 import time
 np.random.seed(1234)
 torch.manual_seed(1234) 
+
+def print_list(text_list, val_list):
+    temp = list(zip(text_list, val_list))
+    for (a, b) in temp:
+        print a, ': ', str(round(b, 2)), '    ',
+    print
 
 def herd_points(probs, num):
     """ Based on Welling & Chen (2010), eqn (18) and (19) """
@@ -25,7 +30,7 @@ def herd_points(probs, num):
 
     return x.float()
 
-def getRelax(G, x, nsamples, influ_obj, herd): 
+def getRelax(dpp, x, nsamples, herd = 0): 
 
     current_sum = Variable(torch.FloatTensor([0]), requires_grad = False) 
 
@@ -35,29 +40,16 @@ def getRelax(G, x, nsamples, influ_obj, herd):
         samples_list = Variable(torch.bernoulli(x.repeat(nsamples, 1)))
 
     for sample in samples_list:
-        current_sum = current_sum + influ_obj(sample.numpy())
+        current_sum = current_sum + dpp(sample)
 
     return current_sum/nsamples
 
-def getCondGrad(grad, k):
-
-    #conditional gradient for cardinality constraints
-
-    N = grad.shape[0]
-    top_k = Variable(torch.zeros(N)) #conditional grad
-    sorted_ind = torch.sort(grad, descending = True)[1][0:k]
-    top_k[sorted_ind] = 1
-#    positive = grad > 0
-#    top_k_positive = (top_k.byte() * positive).float()
-#    return top_k_positive 
-    return top_k
-
-def getGrad(G, x, nsamples, influ_obj, herd):
+def getGrad(dpp, x, nsamples, herd = 0):
 
     #Returns the gradient vector of the multilinear relaxation at x as given in Chekuri's paper
     #(See Theorem 1 in nips2012 paper)
 
-    N = G.number_of_nodes()
+    N = dpp.N 
     grad = Variable(torch.zeros(N))
 
     if herd == 1: 
@@ -70,87 +62,116 @@ def getGrad(G, x, nsamples, influ_obj, herd):
         m = torch.zeros(sample.size()) 
         for p in np.arange(N):
             m[p] = 1
-            grad[p] = grad[p] + (influ_obj(np.logical_or(sample.numpy(), m.numpy())) - influ_obj(np.logical_and(sample.numpy(), np.logical_not(m.numpy()))))
+            a = torch.Tensor(np.logical_or(sample.numpy(), m.numpy()).astype(int))
+            b = torch.Tensor(np.logical_and(sample.numpy(), np.logical_not(m.numpy())).astype(int))
+            grad[p] = grad[p] + (dpp(a) - dpp(b))
             m[p] = 0
     return grad*1.0/nsamples
 
-def runFrankWolfe(G, nsamples, k, log_file, opt_file, num_fw_iter, p, num_influ_iter, if_herd):
+def getCondGrad(grad, k):
 
-    N = nx.number_of_nodes(G)
+    #conditional gradient for cardinality constraints
 
-    x = Variable(torch.Tensor([1.0*k/N]*N))
+    N = grad.shape[0]
+    top_k = Variable(torch.zeros(N)) #conditional grad
+    sorted_ind = torch.sort(grad, descending = True)[1][0:k]
+    neg_ind = grad < 0
+    top_k[sorted_ind] = 1
+    top_k[neg_ind] = 0
+    return top_k
+
+
+def rounding(x, k):
+    rounded_x = torch.zeros(x.shape) #conditional grad
+    sorted_ind = torch.sort(x, descending = True)[1][0:k]
+    rounded_x[sorted_ind] = 1
+    return rounded_x
+
+def prune(dpp, I):
+    #We go through items in I in the order 1...N, and keep items with +ve marginal gain
+
+    items_I = torch.LongTensor([x for x in range(len(I)) if I[x] == 1] )
+    sorted_I_items = torch.sort(items_I)[0] #asceding order
+    current_set = torch.Tensor([0]*dpp.N)
+    for item in sorted_I_items:
+        include_sample = current_set.clone()
+        include_sample[item] = 1
+        marginal_gain = dpp(include_sample) - dpp(current_set) 
+        if marginal_gain > 0:
+            current_set[item] = 1
+    temp = [x for x in range(1, dpp.N) if current_set[x] == 1]
+    print "Items in pruned set = ", len(temp)
+    return current_set
+
+
+def runFrankWolfe(dpp, args, log_file, opt_file, iterates_file, if_herd = 0):
+
+    x = Variable(torch.Tensor([1.0*args.k/args.N]*args.N))
     
     bufsize = 0
 
     f = open(log_file, 'w', bufsize)
 
-    influ_obj = Influence(G, p, num_influ_iter)
-
     tic = time.clock()
 
-    iter_num = 0
-    obj = getRelax(G, x, nsamples, influ_obj, if_herd)
+    mlr = getRelax(dpp, x, args.num_samples_mc)
     toc = time.clock()
 
-    influ_val = []
-    influ_val_best = []
-    influ_best = -10
+    rounded_val = []
+    rounded_val_best = []
+    rounded_best = -10
 
-    print "Iteration: ", iter_num, "    obj = ", obj.item(), "  time = ", (toc - tic),  "   Total/New/Cache: ", influ_obj.itr_total , influ_obj.itr_new , influ_obj.itr_cache
+    text_list = ['Iteration', 'mlr', 'time']
+    val_list = [0, mlr.item(), (toc - tic)]
+    print_list(text_list, val_list)
 
-    f.write(str(toc - tic) + " " + str(obj.item()) + " " + str(influ_obj.itr_total) + '/' + str(influ_obj.itr_new) + '/' + str(influ_obj.itr_cache) + "\n") 
+    f.write(str(toc - tic) + " " + str(mlr.item()) + " " + str(dpp.itr_total) + '/' + str(dpp.itr_new) + '/' + str(dpp.itr_cache) + "\n") 
 
-    for iter_num in np.arange(1, num_fw_iter):
+    for iter_num in np.arange(1, args.num_fw_iter):
 
-        influ_obj.counter_reset()
+        dpp.counter_reset()
 
-        grad = getGrad(G, x, nsamples, influ_obj, if_herd)
+        grad = getGrad(dpp, x, args.num_samples_mc, )
 
-        x_star = getCondGrad(grad, k)
+        x_star = getCondGrad(grad, args.k)
 
         step = 2.0/(iter_num + 2) 
 
         x = step*x_star + (1 - step)*x
 
-        obj = getRelax(G, x, nsamples, influ_obj, if_herd)
+        mlr = getRelax(dpp, x, args.num_samples_mc)
         
         toc = time.clock()
 
-        print "Iteration: ", iter_num, "    obj = ", obj.item(), "  time = ", (toc - tic),  "   Total/New/Cache: ", influ_obj.itr_total , influ_obj.itr_new , influ_obj.itr_cache
+        val_list = [iter_num, mlr.item(), (toc - tic)]
+        print_list(text_list, val_list)
+#        print "Iteration: ", iter_num, "    mlr = ", mlr.item(), "  time = ", (toc - tic),  "   Total/New/Cache: ", dpp.itr_total , dpp.itr_new , dpp.itr_cache
 
-        f.write(str(toc - tic) + " " + str(obj.item()) + " " + str(influ_obj.itr_total) + '/' + str(influ_obj.itr_new) + '/' + str(influ_obj.itr_cache) + "\n") 
-
-        if iter_num % 10 == 0:
-
-            #Round the current solution and get function values
-            top_k = Variable(torch.zeros(N)) #conditional grad
-            sorted_ind = torch.sort(x, descending = True)[1][0:k]
-            top_k[sorted_ind] = 1
-            influ = submodObj(G, top_k, p, 100)
-            influ_val.append(influ)
-            if influ > influ_best:
-                influ_best = influ
-            influ_val_best.append(influ_best)
+        f.write(str(toc - tic) + " " + str(mlr.item()) + " " + str(dpp.itr_total) + '/' + str(dpp.itr_new) + '/' + str(dpp.itr_cache) + "\n") 
 
     f.close()
 
-    x_opt = x
-
     #Round the optimum solution and get function values
-    top_k = Variable(torch.zeros(N)) #conditional grad
-    sorted_ind = torch.sort(x_opt, descending = True)[1][0:k]
-    top_k[sorted_ind] = 1
-    gt_val = submodObj(G, top_k, p, 100)
+    rounded_x = rounding(x, args.k)
 
-    #Save optimum solution and value
+    #Since DPPs are non-monotone, we need to prune the independent set
+    pruned_x = prune(dpp, rounded_x)
+
+    opt_val = dpp(pruned_x) 
+
+    print "Final rounded value = ", opt_val.item()
+
+    #Save optimum value, fractional solution, and rounded+pruned solution 
     f = open(opt_file, 'w')
 
-    for i in range(len(influ_val)):
-        f.write(str(influ_val[i].item()) + ' ' + str(influ_val_best[i].item()) + '\n')
+    f.write(str(opt_val.item()) + '\n')
 
-    f.write(str(gt_val.item()) + '\n')
-    for x_t in x_opt:
-        f.write(str(x_t.item()) + '\n')
+    for t in x:
+        f.write(str(t.item()) + '\n')
+
+    for t in pruned_x:
+        f.write(str(t.item()) + '\n')
+
     f.close()
 
     return x
